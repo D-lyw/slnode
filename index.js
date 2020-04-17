046460abf7a2d965a7536d2fb8fb4df8ed00ec22
D-lyw <liuyuanwang4321@gmail.com>
2020-04-18 00:09:24 +0800

    fix nodejs cli command

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// index.js
+
+const fs = require('fs')
+const path = require('path')
+
+const getCommands = function () {
+    const result = {}
+    fs.readFileSync(path.join(__dirname, './src/command')).forEach(fileName => {
+        const cmdName = path.basename(fileName, '.js')
+        const cmdFun = require(`./src/command/${cmdName}`)
+        result[cmdFun.name] = cmdFun
+    })
+    return result
+}
+
+module.exports = getCommands()
